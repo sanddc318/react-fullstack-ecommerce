04135364fa6c250c273d77e74ccf35c78f3820f8
sanddc318 <sanddc318@gmail.com>
2017-09-16 18:17:09 -0400

    Styled listings and listing overlays.

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { House1 } from '../assets/images/house-1.png'
-import { House2 } from '../assets/images/house-2.png'
-import { House3 } from '../assets/images/house-3.png'
-import { House4 } from '../assets/images/house-4.png'
+import house1 from '../assets/images/house-1.png'
+import house2 from '../assets/images/house-2.png'
+import house3 from '../assets/images/house-3.png'
+import house4 from '../assets/images/house-4.png'
+import avatar from '../assets/images/user-avatar.png'
 
 class Listings extends Component {
   constructor() {
@@ -33,68 +34,219 @@ class Listings extends Component {
             </div>
           </div>
 
-          <div className="listings-wrapper">
-            <div className="listing">
-              <section className="listing-img">
-                <span>302 Mercer Drive</span>
-                <img src={House1} alt="House" />
-              </section>
+          {/* LISTING 1 */}
+          <div className="listing-wrapper">
+            <section className="listing-image">
+              <span>302 Mercer Drive</span>
+              <img src={house1} alt="House" />
+            </section>
 
-              <section className="listing-hover-detail">
-                <div className="user-details">
-                  <div className="user-pic">
-                    <img src="" alt="" />
-                  </div>
-
-                  <div className="user-info">
-                    <span className="user-name">Nina Smith</span>
-                    <span>Posted on </span>
-                    <span className="post-date">09/14/17</span>
-                  </div>
+            <section className="listing-hover-detail">
+              <div className="user-details">
+                <div className="user-pic">
+                  <img src={avatar} alt="User avatar" />
                 </div>
 
-                <div className="listing-hover-details">
-                  <div className="top-row">
-                    <span>
-                      <i className="fa fa-wrench" aria-hidden="true" />
-                      288 m&sup2;
-                    </span>
-                    <span>
-                      <i className="fa fa-bed" aria-hidden="true" />
-                      Unfurnished
-                    </span>
-                  </div>
-
-                  <div className="bottom-row">
-                    <span>
-                      <i className="fa fa-th-large" aria-hidden="true" />7
-                      bedrooms
-                    </span>
-                    <a href="#">view more</a>
-                  </div>
+                <div className="user-info">
+                  <span className="user-name">Nina Smith</span>
+                  <span className="post-date">Posted on 09/14/17</span>
                 </div>
-              </section>
+              </div>
 
-              <section className="listing-details">
-                <span>$900 /month</span>
-                <span>
-                  <i className="fa fa-map-marker" aria-hidden="true" />
-                  Doublegate
-                </span>
-              </section>
-            </div>
+              <div className="listing-hover-info">
+                <div className="top-row">
+                  <span>
+                    <i className="fa fa-wrench" aria-hidden="true" />
+                    288 m&sup2;
+                  </span>
+                  <span>
+                    <i className="fa fa-bed" aria-hidden="true" />
+                    Unfurnished
+                  </span>
+                </div>
+
+                <div className="bottom-row">
+                  <span>
+                    <i className="fa fa-th-large" aria-hidden="true" />7
+                    bedrooms
+                  </span>
+                  <a href="#">view more</a>
+                </div>
+              </div>
+            </section>
+
+            <section className="listing-details">
+              <span className="price">$900 /month</span>
+              <span className="location">
+                <i className="fa fa-map-marker" aria-hidden="true" />
+                Doublegate
+              </span>
+            </section>
           </div>
 
-          <div className="pagination-wrapper">
-            <ul class="pagination-nums">
-              <li>Prev</li>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>Next</li>
-            </ul>
+          {/* LISTING 2 */}
+          <div className="listing-wrapper">
+            <section className="listing-image">
+              <span>302 Mercer Drive</span>
+              <img src={house2} alt="House" />
+            </section>
+
+            <section className="listing-hover-detail">
+              <div className="user-details">
+                <div className="user-pic">
+                  <img src={avatar} alt="User avatar" />
+                </div>
+
+                <div className="user-info">
+                  <span className="user-name">Nina Smith</span>
+                  <span>Posted on </span>
+                  <span className="post-date">09/14/17</span>
+                </div>
+              </div>
+
+              <div className="listing-hover-info">
+                <div className="top-row">
+                  <span>
+                    <i className="fa fa-wrench" aria-hidden="true" />
+                    288 m&sup2;
+                  </span>
+                  <span>
+                    <i className="fa fa-bed" aria-hidden="true" />
+                    Unfurnished
+                  </span>
+                </div>
+
+                <div className="bottom-row">
+                  <span>
+                    <i className="fa fa-th-large" aria-hidden="true" />7
+                    bedrooms
+                  </span>
+                  <a href="#">view more</a>
+                </div>
+              </div>
+            </section>
+
+            <section className="listing-details">
+              <span className="price">$900 /month</span>
+              <span className="location">
+                <i className="fa fa-map-marker fa-lg" aria-hidden="true" />
+                Doublegate
+              </span>
+            </section>
           </div>
+
+          {/* LISTING 3 */}
+          <div className="listing-wrapper">
+            <section className="listing-image">
+              <span>302 Mercer Drive</span>
+              <img src={house3} alt="House" />
+            </section>
+
+            <section className="listing-hover-detail">
+              <div className="user-details">
+                <div className="user-pic">
+                  <img src={avatar} alt="User avatar" />
+                </div>
+
+                <div className="user-info">
+                  <span className="user-name">Nina Smith</span>
+                  <span>Posted on </span>
+                  <span className="post-date">09/14/17</span>
+                </div>
+              </div>
+
+              <div className="listing-hover-info">
+                <div className="top-row">
+                  <span>
+                    <i className="fa fa-wrench" aria-hidden="true" />
+                    288 m&sup2;
+                  </span>
+                  <span>
+                    <i className="fa fa-bed" aria-hidden="true" />
+                    Unfurnished
+                  </span>
+                </div>
+
+                <div className="bottom-row">
+                  <span>
+                    <i className="fa fa-th-large" aria-hidden="true" />7
+                    bedrooms
+                  </span>
+                  <a href="#">view more</a>
+                </div>
+              </div>
+            </section>
+
+            <section className="listing-details">
+              <span className="price">$900 /month</span>
+              <span className="location">
+                <i className="fa fa-map-marker" aria-hidden="true" />
+                Doublegate
+              </span>
+            </section>
+          </div>
+
+          {/* LISTING 4 */}
+          <div className="listing-wrapper">
+            <section className="listing-image">
+              <span>302 Mercer Drive</span>
+              <img src={house4} alt="House" />
+            </section>
+
+            <section className="listing-hover-detail">
+              <div className="user-details">
+                <div className="user-pic">
+                  <img src={avatar} alt="User avatar" />
+                </div>
+
+                <div className="user-info">
+                  <span className="user-name">Nina Smith</span>
+                  <span>Posted on </span>
+                  <span className="post-date">09/14/17</span>
+                </div>
+              </div>
+
+              <div className="listing-hover-info">
+                <div className="top-row">
+                  <span>
+                    <i className="fa fa-wrench" aria-hidden="true" />
+                    288 m&sup2;
+                  </span>
+                  <span>
+                    <i className="fa fa-bed" aria-hidden="true" />
+                    Unfurnished
+                  </span>
+                </div>
+
+                <div className="bottom-row">
+                  <span>
+                    <i className="fa fa-th-large" aria-hidden="true" />7
+                    bedrooms
+                  </span>
+                  <a href="#">view more</a>
+                </div>
+              </div>
+            </section>
+
+            <section className="listing-details">
+              <span className="price">$900 /month</span>
+              <span className="location">
+                <i className="fa fa-map-marker" aria-hidden="true" />
+                Doublegate
+              </span>
+            </section>
+          </div>
+        </div>
+
+        <div className="pagination-wrapper">
+          <ul className="pagination-nums">
+            <li>Prev</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>Next</li>
+          </ul>
         </div>
       </section>
     )
