50dbc8072144c6104c5c064e330f8bc85966979f
sanddc318 <sanddc318@gmail.com>
2017-09-16 16:37:38 -0400

    Styled search bar and sort options.

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -14,85 +14,87 @@ class Listings extends Component {
   render() {
     return (
       <section id="listings">
-        <div className="search-wrapper">
-          <input type="text" name="search" />
+        <div className="inside">
+          <div className="search-wrapper">
+            <input type="text" name="search" placeholder="Enter a city" />
 
-          <div className="results-wrapper">
-            <small>390 results found</small>
+            <div className="results-wrapper">
+              <small>390 results found</small>
 
-            <div className="sort-options">
-              <select name="sort-by" className="sort-results">
-                <option value="price-asc">Lowest Price</option>
-                <option value="price-desc">Highest Price</option>
-              </select>
+              <div className="sort-options">
+                <select name="sort-by" className="sort-results">
+                  <option value="price-asc">Lowest Price</option>
+                  <option value="price-desc">Highest Price</option>
+                </select>
 
-              <i className="fa fa-list" aria-hidden="true" />
-              <i className="fa fa-th" aria-hidden="true" />
+                <i className="fa fa-list" aria-hidden="true" />
+                <i className="fa fa-th" aria-hidden="true" />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="listings-wrapper">
-          <div className="listing">
-            <section className="listing-img">
-              <span>302 Mercer Drive</span>
-              <img src={House1} alt="House" />
-            </section>
+          <div className="listings-wrapper">
+            <div className="listing">
+              <section className="listing-img">
+                <span>302 Mercer Drive</span>
+                <img src={House1} alt="House" />
+              </section>
 
-            <section className="listing-hover-detail">
-              <div className="user-details">
-                <div className="user-pic">
-                  <img src="" alt="" />
+              <section className="listing-hover-detail">
+                <div className="user-details">
+                  <div className="user-pic">
+                    <img src="" alt="" />
+                  </div>
+
+                  <div className="user-info">
+                    <span className="user-name">Nina Smith</span>
+                    <span>Posted on </span>
+                    <span className="post-date">09/14/17</span>
+                  </div>
                 </div>
 
-                <div className="user-info">
-                  <span className="user-name">Nina Smith</span>
-                  <span>Posted on </span>
-                  <span className="post-date">09/14/17</span>
-                </div>
-              </div>
+                <div className="listing-hover-details">
+                  <div className="top-row">
+                    <span>
+                      <i className="fa fa-wrench" aria-hidden="true" />
+                      288 m&sup2;
+                    </span>
+                    <span>
+                      <i className="fa fa-bed" aria-hidden="true" />
+                      Unfurnished
+                    </span>
+                  </div>
 
-              <div className="listing-hover-details">
-                <div className="top-row">
-                  <span>
-                    <i className="fa fa-wrench" aria-hidden="true" />
-                    288 m&sup2;
-                  </span>
-                  <span>
-                    <i className="fa fa-bed" aria-hidden="true" />
-                    Unfurnished
-                  </span>
+                  <div className="bottom-row">
+                    <span>
+                      <i className="fa fa-th-large" aria-hidden="true" />7
+                      bedrooms
+                    </span>
+                    <a href="#">view more</a>
+                  </div>
                 </div>
+              </section>
 
-                <div className="bottom-row">
-                  <span>
-                    <i className="fa fa-th-large" aria-hidden="true" />7
-                    bedrooms
-                  </span>
-                  <a href="#">view more</a>
-                </div>
-              </div>
-            </section>
-
-            <section className="listing-details">
-              <span>$900 /month</span>
-              <span>
-                <i className="fa fa-map-marker" aria-hidden="true" />
-                Doublegate
-              </span>
-            </section>
+              <section className="listing-details">
+                <span>$900 /month</span>
+                <span>
+                  <i className="fa fa-map-marker" aria-hidden="true" />
+                  Doublegate
+                </span>
+              </section>
+            </div>
           </div>
-        </div>
 
-        <div className="pagination-wrapper">
-          <ul class="pagination-nums">
-            <li>Prev</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>Next</li>
-          </ul>
+          <div className="pagination-wrapper">
+            <ul class="pagination-nums">
+              <li>Prev</li>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>Next</li>
+            </ul>
+          </div>
         </div>
       </section>
     )
